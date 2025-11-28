0bee525ee8c9f34987b0295d5184e77ff5d9a5e4
jonzim-cmd <jonzim@posteo.de>
2025-11-28 19:04:44 +0100

    deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/robotics-engineering-simulator', // Assuming the repository name is 'robotics-engineering-simulator'
 };
 
 export default nextConfig;
